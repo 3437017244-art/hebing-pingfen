75c0665dbacae5856eaf30476d69d6b5af95bf3a
3437017244-art <3437017244@qq.com>
2026-07-18 08:28:17 +0800

    主页底部加入弱化版本号与同步入口

--- site-config.js
+++ site-config.js
@@ -2,10 +2,10 @@
   'use strict';
 
   global.HEBING_SITE = {
-    version: '1.0.14',
+    version: '1.0.15',
     repoName: 'hebing-pingfen',
     updatedAt: '2026-07-18',
-    buildId: '20260718-081759',
+    buildId: '20260718-082814',
     githubUser: '3437017244-art',
     // 云端数据同步码（jsonblob）。部署后所有设备自动使用，无需手动复制。
     defaultSyncCode: '019f58bc-163f-7878-9ee6-4066a48180ee',
