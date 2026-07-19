77f73d48f8cab6b61774bcbe6b024cb69db5ac06
3437017244-art <3437017244@qq.com>
2026-07-19 16:26:32 +0800

    修复APP地图顶部招牌触摸不灵敏

--- site-config.js
+++ site-config.js
@@ -2,10 +2,10 @@
   'use strict';
 
   global.HEBING_SITE = {
-    version: '1.0.37',
+    version: '1.0.38',
     repoName: 'hebing-pingfen',
     updatedAt: '2026-07-19',
-    buildId: '20260719-162613',
+    buildId: '20260719-162629',
     githubUser: '3437017244-art',
     // 云端数据同步码（jsonblob）。部署后所有设备自动使用，无需手动复制。
     defaultSyncCode: '019f58bc-163f-7878-9ee6-4066a48180ee',
