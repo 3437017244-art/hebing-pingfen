e9c4d8832833e576ae46276f3b2ee149d0c73ace
3437017244-art <3437017244@qq.com>
2026-07-24 13:26:43 +0800

    去掉有库存店名胶囊底色

--- site-config.js
+++ site-config.js
@@ -2,10 +2,10 @@
   'use strict';
 
   global.HEBING_SITE = {
-    version: '1.0.51',
+    version: '1.0.52',
     repoName: 'hebing-pingfen',
     updatedAt: '2026-07-24',
-    buildId: '20260724-131855',
+    buildId: '20260724-132641',
     githubUser: '3437017244-art',
     // 云端数据同步码（jsonblob）。部署后所有设备自动使用，无需手动复制。
     defaultSyncCode: '019f58bc-163f-7878-9ee6-4066a48180ee',
